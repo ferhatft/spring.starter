32a04c063efbe8d1d9c40a5a8b993be2ea997294
ferhatft <ferhat.ft51@gmail.com>
2024-02-27 00:51:42 +0300

    kurulum ve ilk controller

--- src/main/java/com/turkcell/spring/starter/controller/ExampleController.java
+++ src/main/java/com/turkcell/spring/starter/controller/ExampleController.java
@@ -1,18 +1,31 @@
 package com.turkcell.spring.starter.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/example")
 public class ExampleController {
-
-    @GetMapping
-    public String a(@RequestParam("abc") String name){
-        return  "Merhabe"+name;
+    // ? => Sistem gelen isteği spesifik olarak TEK BİR TANE fonksiyona yönlendirebiliyor ise sorun yok.
+    @GetMapping()
+    public String a(@RequestParam("abc") String name)
+    {
+        return "Merhaba " + name;
     }
-
+    @PostMapping()
+    public Response b(@RequestBody Request request){
+        Response response = new Response();
+        response.setGivenNumber(request.getNumber());
+        return response;
+    }
+    //@PutMapping()
+    //@DeleteMapping
+    @GetMapping("{page}")
+    public String b(@PathVariable String page) {
+        return "Get Request " + page;
+    }
+    @GetMapping("get3")
+    public String c() {
+        return "Get 3 path i";
+    }
 }
---
+++ src/main/java/com/turkcell/spring/starter/controller/Response.java
@@ -0,0 +1,14 @@
+package com.turkcell.spring.starter.controller;
+
+public class Response {
+
+    int givenNumber;
+
+    public int getNumber() {
+        return givenNumber;
+    }
+
+    public void setGivenNumber(int number) {
+        this.givenNumber = number;
+    }
+}
